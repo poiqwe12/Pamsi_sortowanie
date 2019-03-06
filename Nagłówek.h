b15e378cd65110995ac9c0449678c2f5833e92ae
poiqwe12 <46876601+poiqwe12@users.noreply.github.com>
2019-03-06 22:19:48 +0100

    Add files via upload

---
+++ Nagłówek.h
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef _nazwa_pliku_
+#define _nazwa_pliku_
+#include <iostream>
+
+template<typename Typ,int Wymiar> 
+class Dane
+{
+	Typ tab[Wymiar];
+public:
+	const Typ& operator [] (unsigned int index)const
+	{
+		if (index < Wymiar) return tab[index];
+		exit(EXIT_FAILURE);
+	}
+	void Inicjuj_losowo();
+
+
+};
+
+
+template <typename Typ, int Wymiar>
+std::ostream& operator << (std::ostream &Strm, const Dane<Typ, Wymiar> &D)
+{
+	for (int i =0;i<Wymiar;++i)
+	{
+		Strm << D[i] << " ";
+	}
+	return Strm;
+}
+template<typename Typ, int Wymiar>
+void Dane<Typ, Wymiar>::Inicjuj_losowo()
+{
+	for (Typ &i : tab)
+	{
+		i = rand() ;
+	}
+}
+
+
+#endif
